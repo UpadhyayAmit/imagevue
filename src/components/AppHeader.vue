705d831a1343db223f880d449b06889c0ff46327
Amit Upadhyay <upadhyaya.amit@hotmail.com>
2022-11-01 22:23:52 +0530

    multi router setup

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="ui secondary pointing menu">
-    <a class="item active">
+    <router-link to="/" class="item active">
       Image Storage
-    </a>
+    </router-link>
     <div class="right menu">
       <div v-if="isLoggedIn" class="horizontal">
-        <a class="item">Galleries</a>
-        <a class="item">Upload</a>
+        <router-link to="/" class="item">Galleries</router-link>
+        <router-link to="/upload" class="item">Upload</router-link>
         <a class="item" @click="logout">Logout</a>
       </div>
       <a v-else href="#" class="ui item" @click="login">
--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import { createWebHistory, createRouter } from 'vue-router';
 import App from './App.vue';
 import store from './store/index';
 import AuthHandler from './components/AuthHandler.vue';
+import ImageList from './components/ImageList.vue';
+import UploadForm from './components/UploadForm.vue';
 
 // Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/',
+    component: ImageList,
+  },
+  {
+    path: '/upload',
+    component: UploadForm,
+  },
   {
     path: '/oauth2/callback',
     component: AuthHandler,
